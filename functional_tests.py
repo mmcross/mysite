4f22e250017901d7e9bd4fc5dff0a4df7297890a
mmcross <y113355@yeah.net>
2016-01-03 21:06:16 +0800

    使用了unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,16 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
-assert 'django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self):
+		self.browser.close()
+
+	def test_can_start(self):
+		self.assertIn('To-Do',self.browser.title)
+		self.fail('finish')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
