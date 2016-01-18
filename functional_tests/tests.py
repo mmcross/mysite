efedab873726cfb5d299a1f806d05a2869f3c8d9
mmcross <y113355@yeah.net>
2016-01-18 17:30:34 +0800

    first step for css

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,12 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-firefox_capabilities = DesiredCapabilities.FIREFOX
-firefox_capabilities['marionette'] = True
-firefox_capabilities['binary'] = 'C:\\Windows\\System32'
-
-
-
 
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
@@ -18,6 +12,21 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.close()
 
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+				inputbox.location['x'] + inputbox.size['width'] /2,512,delta=5
+			)
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+				inputbox.location['x'] + inputbox.size['width'] /2,512,delta=5
+			)
+
+'''
 	def check_for_row_in_list_table(self,row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
@@ -68,3 +77,4 @@ class NewVisitorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
 		self.assertIn('Buy milk',page_text)
+'''
